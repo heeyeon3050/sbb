cc3dae54daa43de61153bafc30f4655c937e120c
heeyeon3050 <kho200a@naver.com>
2023-03-23 14:38:19 +0900

    QuestionForm에 유효성 체크 적용 & question_form.html에서 오류가 있을 때는 오류내역 보여줌

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,10 +1,13 @@
 package com.mysite.sbb.question;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -12,6 +15,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/question")
 @RequiredArgsConstructor
+//@Validated - @Valid를 사용하기 위해서는 @Validated를 선언해줘야 하는데, Controller에서는 생략한다.
 public class QuestionController {
 
     private final QuestionService questionService;
@@ -30,5 +34,28 @@ public class QuestionController {
         model.addAttribute("question", question);
 
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    // questionForm 변수는 model.addAttribute 없이 바로 뷰에서 접근할 수 있다.
+    // QuestionForm questionForm 써주는 이유, question_form.html 에서 questionForm 변수가 없으면 실행이 안되기 때문에
+    // 빈 객체라도 만든다.
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    // @Valid QuestionForm questionForm
+    // questionForm 값을 바인딩 할 때 유효성 체크를 해라!
+    // questionForm 변수와 bindingResult 변수는 model.addAttribute 없이 바로 뷰에서 접근할 수 있다.
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if ( bindingResult.hasErrors() ) {
+            // question_form.html 실행
+            // 다시 작성하라는 의미로 응답에 폼을 실어서 보냄
+            return "question_form";
+        }
+        questionService.create(questionForm.getSubject(), questionForm.getContent());
+
+        return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
 }
